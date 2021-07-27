568a26b5f3f833d53d7873f7bc2bfd58e83ebaf1
Danussh18 <62802664+Danussh18@users.noreply.github.com>
2021-07-27 14:45:11 +0530

    hello

--- src/test/java/FileCopyingTest.java
+++ src/test/java/FileCopyingTest.java
@@ -8,17 +8,6 @@ import static org.junit.Assert.assertEquals;
 
 public class FileCopyingTest {
 
-    @Test
-    public void checkForCopying() throws IOException {
 
-        String expected =" to change the world and reimagine the future. See this keynote and more from the Hitachi Social\n" +
-                " Hitachi Vantara is a data storage systems provider, previously known as Hitachi Data Systems (HDS).\n" +
-                " Headquartered in Santa Clara, Calif., Hitachi Vantara is a wholly owned subsidiary of Hitachi Ltd.\n" +
-                " Gajen Kandiah became chief executive officer of Hitachi Vantara in July 2020.\n" +
-                " like Aldus PageMaker including versions of Lorem Ipsum. Hitachi\n";
-
-        String actual= readAFile();
-        assertEquals(actual,expected);
-    }
 
 }
